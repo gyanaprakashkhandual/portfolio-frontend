2689d1660a139d3bdbe263e1c8effa388ec1998a
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-03-09 20:28:25 +0530

    Dynamic page ts rendering

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    workerThreads: true,
+    cpus: 4, // use more CPUs for static generation
+  },
 };
 
 export default nextConfig;
